8b9ff35092723c52d3ebdbaf6243ad433bc07900
Henry <Henry>
2014-06-06 15:46:40 +0100

    ODESolvers/seulex/seulex: Tighten check for small step-size

--- src/ODE/ODESolvers/seulex/seulex.C
+++ src/ODE/ODESolvers/seulex/seulex.C
@@ -250,7 +250,7 @@ void Foam::seulex::solve
         firstk = false;
         step.reject = false;
 
-        if (mag(dx) <= mag(x)*SMALL)
+        if (mag(dx) <= mag(x)*sqr(SMALL))
         {
              WarningIn("seulex::solve(scalar& x, scalarField& y, stepState&")
                     << "step size underflow :"  << dx << endl;
