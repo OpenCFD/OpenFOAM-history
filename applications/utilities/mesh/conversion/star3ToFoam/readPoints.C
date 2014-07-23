6d26ac87adfc52ae90ce4befad4795b745db9a9b
mattijs <mattijs>
2014-07-23 16:39:44 +0100

    ENH: star3ToFoam: check for eof

--- applications/utilities/mesh/conversion/star3ToFoam/readPoints.C
+++ applications/utilities/mesh/conversion/star3ToFoam/readPoints.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2014 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -37,6 +37,11 @@ label starMesh::readVtxLabel(IFstream& is)
 
     for (int i=0; i<15; i++)
     {
+        if (!is.good())
+        {
+            return -1;
+        }
+
         is.get(lcs[i]);
     }
 
@@ -85,12 +90,10 @@ void starMesh::readPoints(const scalar scaleFactor)
 
         if (pointsFile.good())
         {
-            label pointLabel;
-
             maxLabel = -1;
             while (pointsFile)
             {
-                pointLabel = readVtxLabel(pointsFile);
+                label pointLabel = readVtxLabel(pointsFile);
 
                 if (!pointsFile) break;
 
