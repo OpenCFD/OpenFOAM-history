5f05343ae29b5c60cefa3eb7ccf2b04a0172d440
mattijs <mattijs>
2015-01-07 10:19:29 +0000

    ENH: displacementMotionSolver: add construct-from-component

--- src/dynamicMesh/motionSolver/displacement/displacementMotionSolver.C
+++ src/dynamicMesh/motionSolver/displacement/displacementMotionSolver.C
@@ -31,6 +31,7 @@ License
 namespace Foam
 {
     defineTypeNameAndDebug(displacementMotionSolver, 0);
+    defineRunTimeSelectionTable(displacementMotionSolver, displacement);
 }
 
 
@@ -157,6 +158,100 @@ Foam::displacementMotionSolver::displacementMotionSolver
                 )
             << exit(FatalError);
     }
+}
+
+
+Foam::displacementMotionSolver::displacementMotionSolver
+(
+    const polyMesh& mesh,
+    const IOdictionary& dict,
+    const pointVectorField& pointDisplacement,
+    const pointIOField& points0,
+    const word& type
+)
+:
+    motionSolver(mesh, dict, type),
+    pointDisplacement_(pointDisplacement),
+    points0_(points0)
+{
+    if (points0_.size() != mesh.nPoints())
+    {
+        FatalErrorIn
+        (
+            "displacementMotionSolver::"
+            "displacementMotionSolver\n"
+            "(\n"
+            "    const polyMesh&,\n"
+            "    const IOdictionary&,\n"
+            "    const pointVectorField&,\n"
+            "    const pointIOField&,\n"
+            "    const word&\n"
+            ")"
+        )   << "Number of points in mesh " << mesh.nPoints()
+            << " differs from number of points " << points0_.size()
+            << " read from file " << points0.filePath()
+            << exit(FatalError);
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Selectors * * * * * * * * * * * * * * * * //
+
+Foam::autoPtr<Foam::displacementMotionSolver>
+Foam::displacementMotionSolver::New
+(
+    const word& solverTypeName,
+    const polyMesh& mesh,
+    const IOdictionary& solverDict,
+    const pointVectorField& pointDisplacement,
+    const pointIOField& points0
+)
+{
+    //const word solverTypeName(solverDict.lookup("solver"));
+
+    Info<< "Selecting motion solver: " << solverTypeName << endl;
+
+    const_cast<Time&>(mesh.time()).libs().open
+    (
+        solverDict,
+        "motionSolverLibs",
+        displacementConstructorTablePtr_
+    );
+
+    if (!displacementConstructorTablePtr_)
+    {
+        FatalErrorIn
+        (
+            "displacementMotionSolver::New(const polyMesh& mesh)"
+        )   << "solver table is empty"
+            << exit(FatalError);
+    }
+
+    displacementConstructorTable::iterator cstrIter =
+        displacementConstructorTablePtr_->find(solverTypeName);
+
+    if (cstrIter == displacementConstructorTablePtr_->end())
+    {
+        FatalErrorIn
+        (
+            "displacementMotionSolver::New(const polyMesh&)"
+        )   << "Unknown solver type "
+            << solverTypeName << nl << nl
+            << "Valid solver types are:" << endl
+            << displacementConstructorTablePtr_->sortedToc()
+            << exit(FatalError);
+    }
+
+    return autoPtr<displacementMotionSolver>
+    (
+        cstrIter()
+        (
+            mesh,
+            solverDict,
+            pointDisplacement,
+            points0
+        )
+    );
 }
 
 
