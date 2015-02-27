9253c5b77fe33be6746c38a565021146d8a3a7c9
Henry <Henry>
2015-02-27 16:26:24 +0000

    icoFoam/nonNewtonianIcoFoam: Update to use pisoControl

--- applications/solvers/incompressible/pisoFoam/pisoFoam.C
+++ applications/solvers/incompressible/pisoFoam/pisoFoam.C
@@ -71,7 +71,6 @@ Description
 int main(int argc, char *argv[])
 {
     #include "setRootCase.H"
-
     #include "createTime.H"
     #include "createMesh.H"
 
@@ -95,7 +94,6 @@ int main(int argc, char *argv[])
         // Pressure-velocity PISO corrector
         {
             // Momentum predictor
-
             fvVectorMatrix UEqn
             (
                 fvm::ddt(U)
