9a1e24cdca3a647b404b84b45a3cb9930084f2c8
mattijs <mattijs>
2015-06-09 16:27:34 +0100

    ENH: checkMesh: path for faceSet/cellSet writing did not include time
    So e.g. faceSets from time 5 would overrdie those of time constant.

--- applications/utilities/mesh/manipulation/checkMesh/checkTools.C
+++ applications/utilities/mesh/manipulation/checkMesh/checkTools.C
@@ -1,3 +1,28 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2015 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
 #include "checkTools.H"
 #include "polyMesh.H"
 #include "globalMeshData.H"
@@ -192,9 +217,11 @@ void Foam::mergeAndWrite
 
     const fileName outputDir
     (
-        Pstream::parRun()
-      ? set.time().path()/".."/"postProcessing"/set.name()
-      : set.time().path()/"postProcessing"/set.name()
+        set.time().path()
+      / (Pstream::parRun() ? ".." : "")
+      / "postProcessing"
+      / mesh.pointsInstance()
+      / set.name()
     );
 
 
@@ -319,12 +346,13 @@ void Foam::mergeAndWrite
         mesh.points()
     );
 
-
     const fileName outputDir
     (
-        Pstream::parRun()
-      ? set.time().path()/".."/"postProcessing"/set.name()
-      : set.time().path()/"postProcessing"/set.name()
+        set.time().path()
+      / (Pstream::parRun() ? ".." : "")
+      / "postProcessing"
+      / mesh.pointsInstance()
+      / set.name()
     );
 
 
